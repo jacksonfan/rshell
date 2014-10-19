468a32c5dea6b29c4c2f9b6404125110c8a2a13d
Jackson Fan <jfan013@ucr.edu>
2014-10-18 21:30:28 -0700

    Created main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
